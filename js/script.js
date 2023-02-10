fed7154b902bfc9bfeff501859a119c97fa27e36
Daniel <110478467+DanielDiacon@users.noreply.github.com>
2023-02-10 16:37:20 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 // menu burger <=>
-//let body = document.querySelector('body');
-//const burger = document.getElementById('burger');
-//const menu = document.getElementById('menu');
-//const logo = document.getElementById('logo');
-//logo.onclick = function () {
-//   logo.classList.toggle('active');
-//};
-//burger.onclick = function () {
-//   burger.classList.toggle('active');
-//   menu.classList.toggle('active');
-//};
-//document.onclick = function (e) {
-//   if (e.target.id !== 'burger' && e.target.classList !== 'menu' && e.target.id !== 'logo') {
-//      burger.classList.remove('active');
-//      menu.classList.remove('active');
-//   }
-//};
+let body = document.querySelector('body');
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+
+burger.onclick = function () {
+   burger.classList.toggle('active');
+   menu.classList.toggle('active');
+   body.classList.toggle('lock');
+};
+document.onclick = function (e) {
+   if (e.target.id !== 'burger' && e.target.classList !== 'menu' && e.target.id !== 'logo') {
+      burger.classList.remove('active');
+      menu.classList.remove('active');
+      body.classList.remove('lock');
+   }
+};
 
 
 // header show <=>
